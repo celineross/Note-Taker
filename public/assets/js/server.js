449e7cb9d710373b72f481ff0c09039db3046efd
Celine Ross <celineross@gmail.com>
2020-10-04 10:12:58 -0500

    added route files, added to server.js

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -1,13 +1,21 @@
-const { response } = require("express");
-//this should be the only file that I'm changing
-//everything else is written
-//connect everything together
-
+//required dependencies
 var express = require("express");
+var path = require("path");
 
+//connect route files
+require("./routes/apiroutes")(app);
+require("./routes/htmlroutes")(app);
+
+//setting up express
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+//data parsing for express
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+
+//start server to begin listening
 app.listen(PORT, function() {
     console.log("Note Taker is listening on PORT " + PORT);
 })
